d4a2279057d8162ebd419822187488c78cfe4ccc
Kunal singh <51798496+kunal17035@users.noreply.github.com>
2023-12-04 22:29:47 +0530

    admin build

--- controllers/servicesController.js
+++ controllers/servicesController.js
@@ -1,7 +1,9 @@
-const serviceModel = require('../models/service')
+const serviceModel = require("../models/service");
 
 exports.createService = async (req, res, next) => {
   try {
+    console.log("reached");
+    console.log(req.body);
     var {
       name,
       startingPrice,
@@ -9,24 +11,25 @@ exports.createService = async (req, res, next) => {
       appHomepage,
       webHomepage,
       categoryId,
-      totalProducts
-    } = req.body
-    var imageUrl = ''
-    imageUrl = req.files[0].filename
+      totalProducts,
+    } = req.body;
+    var imageUrl = "";
+    imageUrl = req.files[0].originalname;
+
+    console.log(imageUrl);
     if (
       !name ||
       !startingPrice ||
       !description ||
       !imageUrl ||
-      !appHomepage ||
-      !webHomepage ||
       !categoryId ||
       !totalProducts
     ) {
       res
         .status(400)
-        .json({ success: false, message: 'All the fields are required' })
+        .json({ success: false, message: "All the fields are required" });
     } else {
+      console.log("reached 2");
       await serviceModel.create({
         name: name,
         startingPrice: startingPrice,
@@ -35,59 +38,60 @@ exports.createService = async (req, res, next) => {
         appHomepage: appHomepage,
         webHomepage: webHomepage,
         categoryId: categoryId,
-        totalProducts: totalProducts
-      })
+        totalProducts: totalProducts,
+      });
+      console.log(serviceModel);
       res
         .status(201)
-        .json({ success: true, message: 'Service created successful' })
+        .json({ success: true, message: "Service created successful" });
     }
   } catch (err) {
-    const error = new Error(err)
-    error.httpStatusCode = 500
-    return next(err)
+    const error = new Error(err);
+    error.httpStatusCode = 500;
+    return next(err);
   }
-}
+};
 
 exports.getAllService = async (req, res, next) => {
   try {
-    const result = await serviceModel.find()
+    const result = await serviceModel.find();
     res
       .status(200)
-      .json({ success: true, message: 'These are all services', data: result })
+      .json({ success: true, message: "These are all services", data: result });
   } catch (err) {
-    const error = new Error(err)
-    error.httpStatusCode = 500
-    return next(err)
+    const error = new Error(err);
+    error.httpStatusCode = 500;
+    return next(err);
   }
-}
+};
 
 exports.getCategoryService = async (req, res, next) => {
   try {
-    const id = req.params.id
-    const result = await serviceModel.find({ categoryId: id })
+    const id = req.params.id;
+    const result = await serviceModel.find({ categoryId: id });
     res
       .status(200)
-      .json({ success: true, message: 'These are all services', data: result })
+      .json({ success: true, message: "These are all services", data: result });
   } catch (err) {
-    const error = new Error(err)
-    error.httpStatusCode = 500
-    return next(err)
+    const error = new Error(err);
+    error.httpStatusCode = 500;
+    return next(err);
   }
-}
+};
 
 exports.updateService = async (req, res, next) => {
   try {
-    const id = req.params.id
+    const id = req.params.id;
     const {
       name,
       startingPrice,
       description,
       appHomepage,
       webHomepage,
-      totalProducts
-    } = req.body
-    var imageUrl = ''
-    imageUrl = req.files[0].filename
+      totalProducts,
+    } = req.body;
+    var imageUrl = "";
+    imageUrl = req.files[0].filename;
     if (
       !name ||
       !startingPrice ||
@@ -99,78 +103,76 @@ exports.updateService = async (req, res, next) => {
     ) {
       res
         .status(400)
-        .json({ success: false, message: 'All the fields are required' })
+        .json({ success: false, message: "All the fields are required" });
     } else {
-      var result = await serviceModel.findOne({ _id: id })
-      result.name = name
-      result.startingPrice = startingPrice
-      result.description = description
-      result.imageUrl = imageUrl
-      result.appHomepage = appHomepage
-      result.totalProducts = totalProducts
-      await result.save()
+      var result = await serviceModel.findOne({ _id: id });
+      result.name = name;
+      result.startingPrice = startingPrice;
+      result.description = description;
+      result.imageUrl = imageUrl;
+      result.appHomepage = appHomepage;
+      result.totalProducts = totalProducts;
+      await result.save();
       res
         .status(201)
-        .json({ success: true, message: 'Service updated successful' })
+        .json({ success: true, message: "Service updated successful" });
     }
   } catch (err) {
-    const error = new Error(err)
-    error.httpStatusCode = 500
-    return next(err)
+    const error = new Error(err);
+    error.httpStatusCode = 500;
+    return next(err);
   }
-}
+};
 
 exports.deleteCategoryService = async (req, res, next) => {
   try {
-    const id = req.params.id
-    await serviceModel.findByIdAndDelete({ _id: id })
+    const id = req.params.id;
+    await serviceModel.findByIdAndDelete({ _id: id });
     res
       .status(200)
-      .json({ success: true, message: 'service deleted successful' })
+      .json({ success: true, message: "service deleted successful" });
   } catch (err) {
-    const error = new Error(err)
-    error.httpStatusCode = 500
-    return next(err)
+    const error = new Error(err);
+    error.httpStatusCode = 500;
+    return next(err);
   }
-}
+};
 
 exports.searchService = async (req, res, next) => {
   try {
-    var search = ''
-    var page = 1
+    var search = "";
+    var page = 1;
     if (req.query.search) {
-      search = req.query.search
-      page = req.query.page
+      search = req.query.search;
+      page = req.query.page;
     }
 
-    var limit = 20
-    const allServices = await serviceModel.count()
-    var num = allServices / limit
-    var fixedNum = num.toFixed()
-    var totalPage = fixedNum
+    var limit = 20;
+    const allServices = await serviceModel.count();
+    var num = allServices / limit;
+    var fixedNum = num.toFixed();
+    var totalPage = fixedNum;
     if (num > fixedNum) {
-      totalPage++
+      totalPage++;
     }
 
     const result = await serviceModel
       .find({
-        $or: [{ name: { $regex: '.*' + search + '.*', $options: 'i' } }]
+        $or: [{ name: { $regex: ".*" + search + ".*", $options: "i" } }],
       })
       .limit(limit * 1)
       .skip((page - 1) * limit)
-      .exec()
+      .exec();
 
-    res
-      .status(200)
-      .json({
-        success: true,
-        message: 'These are all services',
-        data: result,
-        totalPage: totalPage
-      })
+    res.status(200).json({
+      success: true,
+      message: "These are all services",
+      data: result,
+      totalPage: totalPage,
+    });
   } catch (err) {
-    const error = new Error(err)
-    error.httpStatusCode = 500
-    return next(err)
+    const error = new Error(err);
+    error.httpStatusCode = 500;
+    return next(err);
   }
-}
+};
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { check, body } = require("express-validator");
+// const { check, isAdminAuth, body } = require("express-validator");
 const User = require("../models/user");
 const { isAdminAuth } = require("../middleware/auth");
 
@@ -21,22 +21,22 @@ const auth_controller = require("../controllers/auth");
 
 router.post(
   "/create-category",
-
+  isAdminAuth,
   category_controller.postCreateCategory
 );
 router.get(
   "/get-all-category",
-
+  isAdminAuth,
   category_controller.getAllCategory
 );
 router.patch(
   "/update-category/:id",
-
+  isAdminAuth,
   category_controller.updateCategory
 ); // passing object id
 router.delete(
   "/delete-category/:id",
-
+  isAdminAuth,
   category_controller.deleteCategory
 ); // passing object id
 
@@ -45,114 +45,119 @@ router.delete(
 
 router.post(
   "/create-service",
-
-  img_upload,
+  isAdminAuth,
+  img_upload.upload,
+  img_upload.myFun,
   service_controller.createService
 );
-router.get("/get-all-service", service_controller.getAllService);
+router.get("/get-all-service", isAdminAuth, service_controller.getAllService);
 router.get(
   "/get-category-service/:id",
-
+  isAdminAuth,
   service_controller.getCategoryService
 ); // passing category id
 router.patch(
   "/update-service/:id",
-
-  img_upload,
+  isAdminAuth,
+  img_upload.upload,
+  img_upload.myFun,
   service_controller.updateService
 ); // passing object id
 router.delete(
   "/delete-service/:id",
-
+  isAdminAuth,
   service_controller.deleteCategoryService
 ); // passing object id
-router.get("/search-service", service_controller.searchService);
+router.get("/search-service", isAdminAuth, service_controller.searchService);
 
 /////////////////////////////////////////////////
 // Product Routes
 
 router.post(
   "/create-product",
-
-  img_upload,
+  isAdminAuth,
+  img_upload.upload,
+  img_upload.myFun,
   product_controller.createProduct
 );
-router.get("/get-all-product", product_controller.getAllProduct);
+router.get("/get-all-product", isAdminAuth, product_controller.getAllProduct);
 router.get(
   "/get-service-product/:id",
-
+  isAdminAuth,
   product_controller.getServiceProduct
 ); // passing service id
 router.patch(
   "/update-product/:id",
-
-  img_upload,
+  isAdminAuth,
+  img_upload.upload,
+  img_upload.myFun,
   product_controller.updateProduct
 ); // passing object id
 router.delete(
   "/delete-product/:id",
-
+  isAdminAuth,
   product_controller.deleteServiceProduct
 ); // passing object id
 
 ///////////////////////////////////////////////
 // Seller Routes
 
-router.post("/create-seller", seller_controller.createSeller);
-router.get("/get-all-seller", seller_controller.getAllSeller);
-router.patch("/update-seller/:id", seller_controller.updateSeller); // passing object id
+router.post("/create-seller", isAdminAuth, seller_controller.createSeller);
+router.get("/get-all-seller", isAdminAuth, seller_controller.getAllSeller);
+router.patch("/update-seller/:id", isAdminAuth, seller_controller.updateSeller); // passing object id
 router.delete(
   "/delete-seller/:id",
-
+  isAdminAuth,
   seller_controller.deleteSeller
 ); // passing object id
-router.get("/search-seller", seller_controller.searchSeller);
+router.get("/search-seller", isAdminAuth, seller_controller.searchSeller);
 router.patch(
   "/update-seller-status/:id",
-
+  isAdminAuth,
   seller_controller.changeSellerStatus
 ); // passing object id
 
 // User Routes
 
-router.post("/create-user", user_controller.createUser);
-router.get("/get-all-user", user_controller.getAllUser);
+router.post("/create-user", isAdminAuth, user_controller.createUser);
+router.get("/get-all-user", isAdminAuth, user_controller.getAllUser);
 router.patch(
   "/update-user/:id",
-
+  isAdminAuth,
   user_controller.updateUserByAdmin
 ); // passing object id
-router.delete("/delete-user/:id", user_controller.deleteUser); // passing object id
-router.get("/search-user", user_controller.searchUser);
+router.delete("/delete-user/:id", isAdminAuth, user_controller.deleteUser); // passing object id
+router.get("/search-user", isAdminAuth, user_controller.searchUser);
 router.patch(
   "/update-user-status/:id",
-
+  isAdminAuth,
   user_controller.changeUserStatus
 ); // passing object id
 
 // Enquiry Routes
-router.get("/get-all-enquiry", enquiry_controller.getAllEnquiry);
+router.get("/get-all-enquiry", isAdminAuth, enquiry_controller.getAllEnquiry);
 router.delete(
   "/delete-enquiry/:id",
-
+  isAdminAuth,
   enquiry_controller.deleteEnquiry
 );
 
 // Package Routes
 router.post(
   "/create-package",
-
-  img_upload,
+  isAdminAuth,
+  img_upload.upload,
+  img_upload.myFun,
   package_controller.createPackage
 );
 router.get(
   "/get-service-package/:id",
-
+  isAdminAuth,
   package_controller.getServicePackage
 ); //passing service id
 router.delete(
   "/delete-package/:id",
-
+  isAdminAuth,
   package_controller.deletePackage
 ); //passing object id
 
