530e584d45fd22fb61cf4a11ff7ba08c198b3570
Kunal singh <51798496+kunal17035@users.noreply.github.com>
2023-12-07 17:46:17 +0530

    added bookings

--- controllers/payments.js
+++ controllers/payments.js
@@ -30,251 +30,110 @@ const instance = new Razorpay({
   key_secret: razorKeySecret,
 });
 
-async function CartToOrder(
-  razorpay_order_id,
-  razorpay_payment_id,
-  razorpay_signature
-) {
+exports.websiteCodOrder = async (req, res, next) => {
   try {
-    // // Find the user by their user ID
-    // const user = await User.findOne({
-    //   razorPayOrderId: razorpay_order_id,
-    // }).populate("cart"); // Populate the 'cart' field
-    // const cart = await Cart.findOne({ userId: user._id }).populate("items"); // Populate the 'cart' field
-    // if (!user) {
-    //   return res.status(404).json({ message: "User not found." });
-    // }
-    // // Extract cart data from the user's cart
-    // const items = cart.items;
-    // // Create an array to store order items
-    // const orderItems = [];
-    // // Process and add plant items to the order
-    // for (const productItem of items) {
-    //   const sku = await Sku.findById(productItem.skuId).populate("productId");
-    //   if (sku) {
-    //     let item = { ...sku.productId };
-    //     item["price"] = sku.price;
-    //     item["colour"] = productItem.quantity;
-    //     item["size"] = sku.size;
-    //     item["availableIn"] = sku.availableIn;
-    //     orderItems.push({
-    //       type: item.type,
-    //       product: item,
-    //       quantity: productItem.quantity,
+    const userId = req.body.userId;
+    const userAddressId = req.body.userAddressId;
+
+    const user = await User.findById(userId);
+    const cart = await Cart.findOne({ userId: userId }).populate("items"); // Populate the 'cart' field
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
+    const items = cart.items;
+    // Create an array to store order items
+    const orderItems = [];
+    //     // Process and add plant items to the order
+    for (const productItem of items) {
+      const prod = await Products.findById(productItem.productId);
+      if (prod) {
+        orderItems.push({
+          product: prod,
+          quantity: productItem.quantity,
+        });
+      }
+    }
+    const userAddress = await UserAddress.findById(userAddressId);
+    const order = new Order({
+      orderPlatform: "website",
+      paymentType: "COD",
+      orderValue: cart.totalPrice,
+      products: orderItems,
+      user: {
+        userId: user._id,
+        phone: user.phone,
+        name: user.name,
+        address: {
+          addressLine: userAddress.addressLine,
+          pincode: userAddress.pincode,
+          mobile: userAddress.mobile,
+          landmark: userAddress.landmark,
+        },
+      },
+    });
+    //     // // Save the order to the database
+    await order.save();
+    return res.status(200).json(order);
+    //     const newNurseryIds = [
+    //       ...new Set(orderItems.map((item) => item.product.nurseryId.toString())),
+    //     ];
+    //     console.log(
+    //       "nurseryIds ==== ",
+    //       newNurseryIds,
+    //       "count -- ",
+    //       newNurseryIds.length
+    //     );
+    //     const productItems = orderItems
+    //       // .filter((item) => item.type === "Plant")
+    //       .map((item) => ({
+    //         type: item.type,
+    //         product: item.product,
+    //         quantity: item.quantity,
+    //       }));
+    //     // console.log("productItems ====== ", productItems);
+    //     for (const id of newNurseryIds) {
+    //       let productDetails = [];
+    //       const orderedProduct = productItems.filter(
+    //         (ele) => ele.product.nurseryId.toString() === id
+    //       );
+    //       console.log("orderedProduct === ", orderedProduct);
+    //       const totalPrice = orderedProduct.reduce(
+    //         (total, item) => total + item.product.price * item.quantity,
+    //         0
+    //       );
+    //       // console.log("totalPrice ===== ", totalPrice);
+    //       const newValue1 = orderedProduct.map((item) => ({
+    //         type: item.type,
+    //         plantId: item.product._id,
+    //         quantity: item.quantity,
+    //       }));
+    //       // console.log(newValue1);
+    //       productDetails = {
+    //         orderId: order._id,
+    //         products: newValue1,
+    //         nurseryId: new mongoose.Types.ObjectId(id),
+    //         totalPrice: totalPrice,
+    //       };
+    //       trackUserOrder(productDetails);
+    //       console.log("seller order == ", productDetails);
+    //     }
+    //     // retrieving quantity price and name of he product
+    //     const pdf = await getUserInvoice(order);
+    //     // console.log(pdf)
+    //     // Clear the user's cart after creating the order
+    //     cart.items = [];
+    //     cart.totalValue = 0;
+    //     await cart.save();
+    //     return res.status(200).json({
+    //       success: true,
+    //       order: order,
+    //       invoice: pdf,
     //     });
-    //   }
-    // }
-    // const userAddress = await UserAddress.aggregate([
-    //   { $unwind: "addresses"},
-    //   { $match: { userId: user._id, "addresses.defaultAddress": true } },
-    //   { $limit: 1 }
-    // ])
-    // // Create a new order instance
-    // const order = new Order({
-    //   paymentType: "Online payment",
-    //   orderValue: cart.totalValue,
-    //   products: orderItems,
-    //   user: {
-    //     userId: user._id,
-    //     phone: user.phone,
-    //     name: user.name,
-    //     address: userAddress[0],
-    //   },
-    // });
-    // // Save the order to the database
-    // await order.save();
-    // // Clear the user's cart after creating the order
-    // cart.items = [];
-    // cart.totalValue = 0;
-    // await cart.save();
-    // const paymentData = new Payment({
-    //   user: user._id,
-    //   order: order._id,
-    //   razorpay_order_id: razorpay_order_id,
-    //   razorpay_payment_id: razorpay_payment_id,
-    //   razorpay_signature: razorpay_signature,
-    //   amount: order.orderValue,
-    // });
-    // await paymentData.save();
-    // // Send a success response
-    // return { message: "Order created successfully." };
   } catch (err) {
     console.log(err);
     return { message: "error", error: err };
   }
-}
-
-exports.getPaymentKey = async (req, res, next) => {
-  res.status(200).json({ key: razorPayKeyId });
 };
-exports.paymentVerification = async (req, res) => {
-  console.log(req.body);
-  const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
-    req.body;
-
-  const body = razorpay_order_id + "|" + razorpay_payment_id;
-
-  const expectedSignature = crypto
-    .createHmac("sha256", razorKeySecret)
-    .update(body.toString())
-    .digest("hex");
-
-  const isAuthentic = expectedSignature === razorpay_signature;
-  if (isAuthentic) {
-    await CartToOrder(
-      razorpay_order_id,
-      razorpay_payment_id,
-      razorpay_signature
-    );
-    res.redirect(
-      `https://chaperoneplants.com/paymentsuccess?reference=${razorpay_payment_id}`
-    );
-  } else {
-    res.status(400).json({
-      success: false,
-    });
-  }
-};
-
-exports.paymentCheckout = async (req, res, next) => {
-  console.log(req.body);
-  try {
-    const userId = req.body.userId;
-    var options = {
-      amount: Number(req.body.totalValue * 100), // amount in the smallest currency unit
-      currency: "INR",
-      // receipt: "order_rcptid_11",
-    };
-    const order = await instance.orders.create(options);
-    const user = await User.findById(userId);
-    user.razorPayOrderId = order.id;
-    await user.save();
-    console.log(user);
-    res.status(200).json({ message: "sucess true", order: order });
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-// exports.codOrder = async (req, res, next) => {
-//   try {
-//     const userId = req.params.userId;
-//     const user = await User.findById(userId);
-//     const cart = await Cart.findOne({ userId: userId }).populate("items"); // Populate the 'cart' field
-//     // console.log(cart)
-//     if (!user) {
-//       return res.status(404).json({ message: "User not found." });
-//     }
-
-//     // Extract cart data from the user's cart
-//     const items = cart.items;
-//     // Create an array to store order items
-//     const orderItems = [];
-//     // Process and add plant items to the order
-//     for (const productItem of items) {
-//       const sku = await Sku.findById(productItem.skuId).populate("productId");
-
-//       if (sku) {
-//         let item = { ...sku.productId };
-//         item["price"] = sku.price;
-//         item["colour"] = productItem.quantity;
-//         item["size"] = sku.size;
-//         item["availableIn"] = sku.availableIn;
-//         orderItems.push({
-//           type: item.type,
-//           product: item,
-//           quantity: productItem.quantity,
-//         });
-//       }
-//     }
-
-//     const userAddress = await UserAddress.aggregate([
-//       { $unwind: "addresses" },
-//       { $match: { userId: user._id, "addresses.defaultAddress": true } },
-//       { $limit: 1 },
-//     ]);
-//     // Create a new order instance
-
-//     const order = new Order({
-//       paymentType: "COD",
-//       orderValue: cart.totalValue,
-//       products: orderItems,
-//       user: {
-//         userId: user._id,
-//         phone: user.phone,
-//         name: user.name,
-//         address: userAddress[0],
-//       },
-//     });
-
-//     // // Save the order to the database
-
-//     await order.save();
-//     const newNurseryIds = [
-//       ...new Set(orderItems.map((item) => item.product.nurseryId.toString())),
-//     ];
-//     console.log(
-//       "nurseryIds ==== ",
-//       newNurseryIds,
-//       "count -- ",
-//       newNurseryIds.length
-//     );
-//     const productItems = orderItems
-//       // .filter((item) => item.type === "Plant")
-//       .map((item) => ({
-//         type: item.type,
-//         product: item.product,
-//         quantity: item.quantity,
-//       }));
-
-//     // console.log("productItems ====== ", productItems);
-
-//     for (const id of newNurseryIds) {
-//       let productDetails = [];
-//       const orderedProduct = productItems.filter(
-//         (ele) => ele.product.nurseryId.toString() === id
-//       );
-//       console.log("orderedProduct === ", orderedProduct);
-//       const totalPrice = orderedProduct.reduce(
-//         (total, item) => total + item.product.price * item.quantity,
-//         0
-//       );
-//       // console.log("totalPrice ===== ", totalPrice);
-//       const newValue1 = orderedProduct.map((item) => ({
-//         type: item.type,
-//         plantId: item.product._id,
-//         quantity: item.quantity,
-//       }));
-//       // console.log(newValue1);
-//       productDetails = {
-//         orderId: order._id,
-//         products: newValue1,
-//         nurseryId: new mongoose.Types.ObjectId(id),
-//         totalPrice: totalPrice,
-//       };
-//       trackUserOrder(productDetails);
-//       console.log("seller order == ", productDetails);
-//     }
-
-//     // retrieving quantity price and name of he product
-
-//     const pdf = await getUserInvoice(order);
-//     // console.log(pdf)
-//     // Clear the user's cart after creating the order
-//     cart.items = [];
-//     cart.totalValue = 0;
-//     await cart.save();
-
-//     return res.status(200).json({
-//       success: true,
-//       order: order,
-//       invoice: pdf,
-//     });
-//   } catch (err) {
-//     console.log(err);
-//     return { message: "error", error: err };
-//   }
-// };
 
 exports.AppcodOrder = async (req, res, next) => {
   try {
