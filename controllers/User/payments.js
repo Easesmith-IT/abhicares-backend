455a481d1003cdd566e86a6719e073eb1c4228a4
dev04lkomavyax <dev04lko@mavyax.com>
2024-01-02 15:38:52 +0530

    code updated

--- controllers/User/payments.js
+++ controllers/User/payments.js
@@ -467,7 +467,42 @@ exports.checkout=async(req,res)=>{
 
 exports.paymentVerification=async(req,res)=>{
   try{
-   res.status(200).json({success:true,message:"varification successful"})
+      
+    const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+    req.body;
+
+  const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+  const expectedSignature = crypto
+    .createHmac("sha256", process.env.RAZORPAY_API_SECRET)
+    .update(body.toString())
+    .digest("hex");
+
+  const isAuthentic = expectedSignature === razorpay_signature;
+
+  if (isAuthentic) {
+    // Database comes here
+
+    // await Payment.create({
+    //   razorpay_order_id,
+    //   razorpay_payment_id,
+    //   razorpay_signature,
+    // });
+
+    // res.redirect(
+    //   `http://localhost:3000/paymentsuccess?reference=${razorpay_payment_id}`
+    // );
+
+    res.status(200).json({success:true,message:"varification successful"})
+  } else {
+    res.status(400).json({
+      success: false,
+      message:"verification failed"
+    });
+  }
+
+
+   
   }catch(err){
    res.status(400).json({success:false,message:"internal server error"})
   } 
