f040199e57d586315e60a851f4e14012312865b4
dev04lkomavyax <dev04lko@mavyax.com>
2024-01-05 12:11:19 +0530

    code updated

--- controllers/Admin/content.js
+++ controllers/Admin/content.js
@@ -49,7 +49,7 @@ exports.uploadBanners = async (req, res, next) => {
 
 exports.getBanners = async (req, res, next) => {
   try {
-    if (req.perm.content === 'write' || req.perm.content === 'read') {
+    // if (req.perm.content === 'write' || req.perm.content === 'read') {
       const { type, section, page, heroBanners } = req.query
       let doc
       if (heroBanners) {
@@ -61,13 +61,13 @@ exports.getBanners = async (req, res, next) => {
           success: true,
           banners: banners
         })
-      } else {
-        doc = await Content.findOne({ type, section, page })
-        res.status(200).json({
-          success: true,
-          banners: doc.image
-        })
-      }
+      // } else {
+      //   doc = await Content.findOne({ type, section, page })
+      //   res.status(200).json({
+      //     success: true,
+      //     banners: doc.image
+      //   })
+      // }
 
       if (!doc) {
         throw new AppError(400, 'Document does not exists')
