f110c89984097bf8218bf4e6a36e8b286133e0b4
easesmith <tech@mavyax.com>
2024-01-19 16:52:03 +0530

    updated code

--- controllers/Admin/content.js
+++ controllers/Admin/content.js
@@ -142,3 +142,61 @@ exports.getContent = async (req, res) => {
     })
   }
 }
+
+exports.getSeoByPage = async (req, res) => {
+  try {
+    const page = req.query.page;
+    const content = await Content.findOne({ page: page, type:'seo' });
+    if (!content) {
+      return res.status(404).json({
+        message: `No content found for the page ${page}`,
+      });
+    }
+    res.status(200).json({
+      seo: content,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: "Error fetching seo",
+      error: err.message,
+    });
+  }
+};
+
+exports.updateSeo = async (req, res) => {
+  try {
+    const { page, seoTitle, seoDescription } = req.body;
+    const content = await Content.findOne({ page: page, type: "seo" });
+    
+
+    if (!content) {
+      await Content.create(req.body)
+      res.status(200);
+    }
+
+    // const updateContent = {
+    //   seoTitle: seoTitle,
+    //   seoDescription: seoDescription,
+    // };
+
+    // const update = await Content.findOneAndUpdate({ _id: id }, updateContent, {
+    //   new: true,
+    // });
+
+    // if (!update) {
+    //   return res.status(400).json({
+    //     message: "Could not update Content",
+    //   });
+    // }
+
+    // res.status(200).json({
+    //   message: "updated successfully",
+    // });
+  }
+   catch (err) {
+    return res.status(500).json({
+      message: "Error while updating content",
+      error: err.message,
+    });
+  }
+};
