cfc52d18ccf9936ab36dd8c011d2c3df76d9aa05
Kunal singh <51798496+kunal17035@users.noreply.github.com>
2024-01-20 02:07:08 +0530

    app code

--- controllers/Admin/sellerOrderController.js
+++ controllers/Admin/sellerOrderController.js
@@ -4,19 +4,16 @@ const AppError = require("../Admin/errorController");
 
 exports.getSellerList = async (req, res, next) => {
   try {
-      const id = req.params.id // this is service id
-      const result = await sellerModel.find({
-        status: "active",
-        "services.serviceId": id,
-      });
-      res.status(200).json({
-        success: true,
-        message: 'Active seller list',
-        data: result
-      })
-    } else {
-      throw new AppError(400, 'You are not authorized')
-    }
+    const id = req.params.id; // this is service id
+    const result = await sellerModel.find({
+      status: "active",
+      "services.serviceId": id,
+    });
+    res.status(200).json({
+      success: true,
+      message: "Active seller list",
+      data: result,
+    });
   } catch (err) {
     next(err);
   }
@@ -24,24 +21,19 @@ exports.getSellerList = async (req, res, next) => {
 
 exports.allotSeller = async (req, res, next) => {
   try {
-    if (req.perm.partners === 'write') {
-      const id = req.params.id // this is seller id 
-      const {bookingId}=req.body
-      if(!bookingId){
-        throw new AppError(400, 'All the fields are required')
-      }     
-        var bookingData= await bookingModel.findOne({_id:bookingId})
-        bookingData.sellerId=id
-        bookingData.status="alloted"
-        await bookingData.save()
-        res.status(200).json({
-          success: true,
-          message: 'Seller order created successful'
-        })
-      
-    } else {
-      throw new AppError(400, 'You are not authorized')
+    const id = req.params.id; // this is seller id
+    const { bookingId } = req.body;
+    if (!bookingId) {
+      throw new AppError(400, "All the fields are required");
     }
+    var bookingData = await bookingModel.findOne({ _id: bookingId });
+    bookingData.sellerId = id;
+    bookingData.status = "alloted";
+    await bookingData.save();
+    res.status(200).json({
+      success: true,
+      message: "Seller order created successful",
+    });
   } catch (err) {
     console.log("err", err);
     next(err);
@@ -49,88 +41,67 @@ exports.allotSeller = async (req, res, next) => {
 };
 
 exports.updateSellerOrderStatus = async (req, res, next) => {
-    try {
-      if (req.perm.partners === 'write') {
-        const id = req.params.id // booking id
-        const {status}=req.body
-        var result=await bookingModel.findOne({_id:id}) 
-        result.status=status
-        await result.save()
+  try {
+    const id = req.params.id; // booking id
+    const { status } = req.body;
+    var result = await bookingModel.findOne({ _id: id });
+    result.status = status;
+    await result.save();
 
-        res.status(200).json({
-          success: true,
-          message: 'Seller order updated successful'
-        })
-      } else {
-        throw new AppError(400, 'You are not authorized')
-      }
-    } catch (err) {
-      next(err)
-    }
+    res.status(200).json({
+      success: true,
+      message: "Seller order updated successful",
+    });
+  } catch (err) {
+    next(err);
   }
 };
 
-  exports.getSellerOrder = async (req, res, next) => {
-    try {
-      if (req.perm.partners === 'write' || req.perm.partners === 'read') {
-        const id = req.params.id // seller id
-        const result =await bookingModel.find({"sellerId":id}).populate({
-          path: 'package',
-          populate: {
-            path: "products",
-            populate: {
-              path: "productId",
-              model: "Product",
-            },
-          },
-        })
-        .populate("userId", "-password");
-
-        res.status(200).json({
-          success: true,
-          message: 'Your order list',
-          sellerOrders:result
-        })
-      } else {
-        throw new AppError(400, 'You are not authorized')
-      }
-    } catch (err) {
-      next(err)
-    }
+exports.getSellerOrder = async (req, res, next) => {
+  try {
+    const id = req.params.id; // seller id
+    const result = await bookingModel.find({ sellerId: id }).populate({
+      path: "package",
+      populate: {
+        path: "products",
+        populate: {
+          path: "productId",
+          model: "Product",
+        },
+      },
+    });
+  } catch (err) {
+    next(err);
   }
 };
 
-  exports.getSellerOrderByStatus = async (req, res, next) => {
-    try {
-      if (req.perm.partners === 'write' || req.perm.partners === 'read') {
-       
-        const id = req.params.id // seller id
-        const {status}=req.body
-        if(!status){
-          throw new AppError(400, 'All the fields are required')
-        }
-        const result =await bookingModel.find({"sellerId":id,status:status}).populate({
-          path: 'package',
-          populate: {
-            path: "products",
-            populate: {
-              path: "productId",
-              model: "Product",
-            },
-          },
-        })
-        .populate("userId", "-password");
-
-        res.status(200).json({
-          success: true,
-          message: 'Your order list',
-          sellerOrders:result
-        })
-      } else {
-        throw new AppError(400, 'You are not authorized')
-      }
-    } catch (err) {
-      next(err)
+exports.getSellerOrderByStatus = async (req, res, next) => {
+  try {
+    const id = req.params.id; // seller id
+    const { status } = req.body;
+    if (!status) {
+      throw new AppError(400, "All the fields are required");
     }
+    const result = await bookingModel
+      .find({ sellerId: id, status: status })
+      .populate({
+        path: "package",
+        populate: {
+          path: "products",
+          populate: {
+            path: "productId",
+            model: "Product",
+          },
+        },
+      })
+      .populate("userId", "-password");
+
+    res.status(200).json({
+      success: true,
+      message: "Your order list",
+      sellerOrders: result,
+    });
+  } catch (err) {
+    next(err);
   }
 };
