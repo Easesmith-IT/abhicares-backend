255994a50d4f732f1d2c130b72c7c201fa4a3c7e
easesmith <tech@mavyax.com>
2025-01-05 17:27:31 +0530

    update code

--- connection/expressMain_connection.js
+++ connection/expressMain_connection.js
@@ -19,9 +19,9 @@ app.use("/api/admin", adminRoute);
 app.use("/api/app", appRoute);
 app.use("/api/service-app", serviceAppRoute);
 app.use("/api/shopping", shoppingRoute);
-// app.use("/api/content", contentRoute);
+app.use("/api/content", contentRoute);
 
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // app.get("/admin", (req, res) => {
 //   return res.sendFile(path.resolve(__dirname, "../", "admin", "index.html"));
