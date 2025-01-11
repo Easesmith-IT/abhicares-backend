f58db81f9455350f915f6e4d8af43794a96e0cde
easesmith <tech@mavyax.com>
2025-01-11 10:56:31 +0530

    commit message

--- util/toMongodbId.js
+++ util/toMongodbId.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 
-exports.toObjectId(id){
-    try {
-        return new mongoose.Types.ObjectId(id);
-    } catch (error) {
-        console.error("Invalid ObjectId:", error);
-        return null;
-    }
+exports.toObjectId = (id) => {
+  try {
+    return new mongoose.Types.ObjectId(id);
+  } catch (error) {
+    console.error("Invalid ObjectId:", error);
+    return null;
+  }
 };
-
