df36da197761517d6fd441d4aa617012ce6b0db3
Aditya Sharma <dev02lko@mavyax.com>
2025-01-11 16:21:20 +0530

    pushed

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2932,7 +2932,7 @@ exports.updateReferAndEarnAmt = catchAsync(async (req, res, next) => {
 //     }
 // };
 
-exports.sendNotificationToAll = async (req, res) => {
+exports.sendNotificationToAll = async (req, res,next) => {
   const { description, date, time, title } = req.body;
   console.log("req.body", req.body);
   // Validate input
